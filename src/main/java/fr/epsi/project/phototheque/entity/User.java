d0feea3b79d84f86b87655deb83afabe74053d64
gmeline <melinegodefroy.pro@gmail.com>
2024-02-21 15:49:31 +0100

    maj user

--- src/main/java/fr/epsi/project/phototheque/entity/User.java
+++ src/main/java/fr/epsi/project/phototheque/entity/User.java
@@ -31,38 +31,30 @@ public class User {
     private String firstname;
     private String lastname;
 
+    private String pseudo;
+    private String password;
+
+    private boolean rank_user;
+    public Long getId() {return id;}
+
+    public void setId(Long id) {this.id = id;}
+
+    public String getFirstname() {return firstname;}
+
+    public void setFirstname(String firstname) {this.firstname = firstname;}
+
+    public String getLastname() {return lastname;}
+
+    public void setLastname(String lastname) {this.lastname = lastname;}
+    public String getPseudo() {return pseudo;}
+
+    public void setPseudo(String pseudo) {this.pseudo = pseudo;}
+
     public String getPassword() {return password;}
 
     public void setPassword(String password) {this.password = password;}
 
-    private String password;
+    public boolean isRank_user() {return rank_user;}
 
-    private boolean rank_user;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
- public boolean isRank_user() {return rank_user;}
-
-public void setRank_user(boolean rank_user) {this.rank_user = rank_user;}
+    public void setRank_user(boolean rank_user) {this.rank_user = rank_user;}
 }
